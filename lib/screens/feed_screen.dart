8c3adbcbb75eeee3f8f77cbe5098e88e811126e0
Aayush Regmi <aayush.181509@ncit.edu.np>
2022-01-16 19:46:48 +0545

    Successfully created feed page

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/images/ic_instagram.svg',
+          color: primaryColor,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.message_outlined),
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => Container(
+                    child: PostCard(
+                      snap: snapshot.data!.docs[index].data(),
+                    ),
+                  ));
+        },
+      ),
+    );
+  }
+}
